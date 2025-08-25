6b2a02b7b62fd534248bf3210a3292abce414ed6
Niloy Debnath <niloydebnathofficial@gmail.com>
2025-08-26 04:54:06 +0600

    now all items are seen in cart list

--- scripts/index.js
+++ scripts/index.js
@@ -15,12 +15,75 @@ function addingToCart(btnId, amountId) {
 
     let totalPrice = parseInt(getElementById("total-price").innerText);
 
-    //   console.log(totalPrice);
-
     totalPrice = totalPrice + amount;
 
     getElementById("total-price").innerText = totalPrice;
-    //   console.log(totalPrice);
+    const cardListContainer = getElementById("cart-list-container");
+
+    const newCart1 = document.createElement("div");
+    const newCart2 = document.createElement("div");
+    const newCart3 = document.createElement("div");
+
+    newCart1.innerHTML = `
+    <div
+              class="p-4 flex justify-center items-center w-60 bg-base-100 shadow-sm"
+            >
+              <figure class="bg-[#1111110d] p-10">
+                <img src="./assets/kitchen-1.png" alt="Shoes" />
+              </figure>
+              <div class="">
+                <h2 class="text-center">K. Accessories</h2>
+                <p class="text-center text-[#11111180]">
+                  <span id="kitchen-cart-amount-3">49.00 TK</span>
+                </p>
+
+                <div class="card-actions justify-end"></div>
+              </div>
+            </div>
+    `;
+    newCart2.innerHTML = `
+    <div
+              class="p-4 flex justify-center items-center w-60 bg-base-100 shadow-sm"
+            >
+              <figure class="bg-[#1111110d] p-10">
+                <img src="./assets/kitchen-2.png" alt="Shoes" />
+              </figure>
+              <div class="">
+                <h2 class="text-center">K. Accessories</h2>
+                <p class="text-center text-[#11111180]">
+                  <span id="kitchen-cart-amount-3">49.00 TK</span>
+                </p>
+
+                <div class="card-actions justify-end"></div>
+              </div>
+            </div>
+    `;
+    newCart3.innerHTML = `
+    <div
+              class="p-4 flex justify-center items-center w-60 bg-base-100 shadow-sm"
+            >
+              <figure  class="bg-[#1111110d] p-10">
+                <img src="./assets/kitchen-3.png" alt="Shoes" />
+              </figure>
+              <div class="">
+                <h2 class="text-center">Home Cooker</h2>
+                <p class="text-center text-[#11111180]">
+                  <span id="kitchen-cart-amount-3">49.00 TK</span>
+                </p>
+
+                <div class="card-actions justify-end"></div>
+              </div>
+            </div>
+    `;
+    if (btnId === "kitchen-cart-button-1") {
+      cardListContainer.append(newCart1);
+    }
+    if (btnId === "kitchen-cart-button-2") {
+      cardListContainer.append(newCart2);
+    }
+    if (btnId === "kitchen-cart-button-3") {
+      cardListContainer.append(newCart3);
+    }
   });
 }
 
